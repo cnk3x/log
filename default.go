908a523e5469d514c1c9e0bd2a3262ecd5346e75
树先生 <hi@shu.run>
2020-04-03 00:34:33 +0800

    rename

--- default.go
+++ default.go
@@ -2,29 +2,31 @@ package log
 
 import "os"
 
-var std = New()
+var standard = Standard()
 
-func Set(l L) {
-	std = l
+const depth = 2
+
+func Set(logger L) {
+	standard = logger
 }
 
 func Config(level, name string, caller, showTime bool) {
-	std.Config(ParseLevel(level), name, caller, showTime)
+	standard.Config(ParseLevel(level), name, caller, showTime)
 }
 
 func Debugf(format string, args ...interface{}) {
-	_ = std.Output(2, DebugLevel, format, args...)
+	_ = standard.Output(depth, DebugLevel, format, args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	_ = std.Output(2, InfoLevel, format, args...)
+	_ = standard.Output(depth, InfoLevel, format, args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	_ = std.Output(2, ErrorLevel, format, args...)
+	_ = standard.Output(depth, ErrorLevel, format, args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	_ = std.Output(2, ErrorLevel, format, args...)
+	_ = standard.Output(depth, ErrorLevel, format, args...)
 	os.Exit(1)
 }
--- logger.go
+++ logger.go
@@ -1,45 +1,56 @@
 package log
 
 import (
-	"strconv"
-	"strings"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
 )
 
-const (
-	DebugLevel Level = 2 << iota
-	InfoLevel
-	ErrorLevel
-)
-
-type L interface {
-	Output(depth int, level Level, format string, args ...interface{}) error
-	Config(level Level, name string, report, showTime bool)
-}
-
-type Level int
-
-func (l Level) String() string {
-	switch l {
-	case DebugLevel:
-		return "DEBUG"
-	case InfoLevel:
-		return "INFO"
-	case ErrorLevel:
-		return "ERROR"
-	default:
-		return strconv.Itoa(int(l))
+func Standard() L {
+	return &logger{
+		l:      log.New(os.Stderr, "", 0),
+		level:  DebugLevel,
+		name:   "shu.run",
+		caller: true,
+		time:   true,
 	}
 }
 
-func ParseLevel(l string) Level {
-	switch strings.ToUpper(l) {
-	case "DEBUG", "DEBU", "D", "0", "2":
-		return DebugLevel
-	case "INFO", "I", "4":
-		return InfoLevel
-	case "ERROR", "ERRO", "ERR", "E", "8":
-		return ErrorLevel
-	default:
-		return InfoLevel
-	}
+type logger struct {
+	l      *log.Logger
+	name   string
+	caller bool
+	time   bool
+	level  Level
 }
+
+func (l *logger) Config(level Level, name string, caller, showTime bool) {
+	l.caller = caller
+	l.time = showTime
+	l.level = level
+	l.name = name
+}
+
+func (l *logger) Output(depth int, level Level, format string, args ...interface{}) error {
+	if level >= l.level {
+
+		prefix := fmt.Sprintf("%s.%s ", l.name, level.String()[:4])
+		if l.time {
+			prefix += fmt.Sprintf("[%s] ", time.Now().In(time.FixedZone("CST", 8*3600)).Format("2006/01/02 15:04:05"))
+		}
+
+		if l.caller {
+			_, f, l, _ := runtime.Caller(depth)
+			_, n := filepath.Split(f)
+			prefix += fmt.Sprintf("(%s:%d) ", n, l)
+		}
+
+		return l.l.Output(depth, prefix+fmt.Sprintf(format, args...))
+	}
+	return nil
+}
+
+var _ L = (*logger)(nil)
